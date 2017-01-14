22c889b55881b1ef67b95e765e1ac1b9c28e09fd
Diogo Pedreira <diogopedreira@outlook.com>
2017-01-14 21:30:27 +0000

    Finished learnyounode myfirstio tutorial

--- nodeschool/learnyounode/3-myfirstio/program.js
+++ nodeschool/learnyounode/3-myfirstio/program.js
@@ -1,1 +1,28 @@
-console.log('HELLO')
+"use strict";
+
+var fs = require('fs');
+
+const PROCESS_ARGV_ARGUMENTS_START_INDEX = 2;
+
+function isFilePathValid(filePath) {
+
+    return filePath !== null && fs.existsSync(filePath);
+}
+
+function countFileLines(filePath) {
+
+    let lineNumber = null;
+
+    if (isFilePathValid(filePath)) {
+
+        let buffer = fs.readFileSync(filePath);
+
+        let fileContent = buffer.toString();
+
+        lineNumber = fileContent.split("\n").length - 1;
+    }
+
+    console.log(lineNumber);
+}
+
+countFileLines(process.argv[PROCESS_ARGV_ARGUMENTS_START_INDEX]);
